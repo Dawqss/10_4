09eff3fdbf60404a2f783813c2075d1bb3bfa10c
Dawid Wyrkowski <dawidwyrkowski@gmail.com>
2016-11-15 01:54:38 +0100

    simple objects

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function Telefon(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+};
+
+Telefon.prototype.printInfo = function() {
+	console.log('Smartphone brand: ' +this.brand);
+	console.log('Smartphone color: ' +this.color);
+	console.log('Smartphone price: ' +this.price +' $')
+};
+
+var iPhone11 = new Telefon ('Apple', 9999, 'white'),
+	theBomb = new Telefon ('Samsung', 999, 'black'),
+	Shell = new Telefon ('Microsoft', 500, 'pink');
